e365cb51059c94b90e089e5a341bc808601fadac
BFO92310 <bfortune@extia.fr>
2017-11-04 06:49:40 -0700

    Add files via upload

--- fonctions.py
+++ fonctions.py
@@ -17,14 +17,13 @@ camera=PiCamera()
 
 camera.start_preview()
 sleep(2)
-camera.capture(photo_prise)
+camera.capture('/home/pi/Desktop/BFO/img.jpg')
 camera.stop_preview()
 
 
 #on enregistre la photo prise sur le bucket S3
-def upload_photo(photo_prise):
-	s3=boto3.resource('s3')
-	s3.Bucket(bucket).upload_file(photo, photo_aws)
+s3=boto3.resource('s3')
+s3.Bucket('picturesbfo').upload_file('img.jpg', 'photo_aws.jpg')
 
 
 #La photo est analysée pour voir si elle correspond à un visage existant dans la collection
@@ -46,51 +45,49 @@ prenom= response['FaceMatches'][0]['Face']['ExternalImageId']
 
 #On choisit une phrase à transmettre à polly avec le prénom lié au visage détecté
 
-def choix_phrase():
-	if not a:
-		phrase=choice(phrase_copains).format(prenom)
-	else:
-		phrase=choice(phrase_inconnu)
+if not a:
+    phrase=choice(phrase_copains).format(prenom)
+else:
+	phrase=choice(phrase_inconnu)
 
 #Synthétisation de la voix avec Polly
-def retour_vocal():
-	voix= boto3.client("polly")
+voix= boto3.client("polly")
 
-	try:
-    	# Request speech synthesis
-    	response = voix.synthesize_speech(Text="<speak>Hi </speak>".format(prenom), TextType='ssml', OutputFormat="mp3",
+try:
+   	# Request speech synthesis
+   	response = voix.synthesize_speech(Text="<speak>Hi </speak>".format(prenom), TextType='ssml', OutputFormat="mp3",
                                         VoiceId="Emma")
-	except (BotoCoreError, ClientError) as error:
-    	# The service returned an error, exit gracefully
-    	print(error)
-    	sys.exit(-1)
+except (BotoCoreError, ClientError) as error:
+   	# The service returned an error, exit gracefully
+   	print(error)
+   	sys.exit(-1)
 
-	# Access the audio stream from the response
-	if "AudioStream" in response:
-    	# Note: Closing the stream is important as the service throttles on the
-    	# number of parallel connections. Here we are using contextlib.closing to
-    	# ensure the close method of the stream object will be called automatically
-    	# at the end of the with statement's scope.
-    	with closing(response["AudioStream"]) as stream:
-        	output = os.path.join(gettempdir(), "speech.mp3")
+# Access the audio stream from the response
+if "AudioStream" in response:
+   	# Note: Closing the stream is important as the service throttles on the
+   	# number of parallel connections. Here we are using contextlib.closing to
+   	# ensure the close method of the stream object will be called automatically
+   	# at the end of the with statement's scope.
+   	with closing(response["AudioStream"]) as stream:
+       	output = os.path.join(gettempdir(), "speech.mp3")
 
-        	try:
-            	# Open a file for writing the output as a binary stream
-            	with open(output, "wb") as file:
-                	file.write(stream.read())
-        	except IOError as error:
-            	# Could not write to file, exit gracefully
-            	print(error)
-            	sys.exit(-1)
+       	try:
+           	# Open a file for writing the output as a binary stream
+           	with open(output, "wb") as file:
+               	file.write(stream.read())
+       	except IOError as error:
+           	# Could not write to file, exit gracefully
+           	print(error)
+           	sys.exit(-1)
 
-	else:
-    # The response didn't contain audio data, exit gracefully
-    sys.exit(-1)
+else:
+   # The response didn't contain audio data, exit gracefully
+   sys.exit(-1)
 
-	# Play the audio using the platform's default player
-	if sys.platform == "win32":
-    	os.startfile(output)
-	else:
-    	# the following works on Mac and Linux. (Darwin = mac, xdg-open = linux).
-    	opener = "open" if sys.platform == "darwin" else "xdg-open"
-	subprocess.call([opener, output])
+# Play the audio using the platform's default player
+if sys.platform == "win32":
+  	os.startfile(output)
+else:
+   	# the following works on Mac and Linux. (Darwin = mac, xdg-open = linux).
+   	opener = "open" if sys.platform == "darwin" else "xdg-open"
+subprocess.call([opener, output])
